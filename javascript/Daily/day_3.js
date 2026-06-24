7e0a74d365a560fc3f1de39f276f7f50a34ab5c9
Awanish Verma <a2023cse9230@imsec.ac.in>
2026-06-24 14:40:40 +0530

    Add JavaScript assignment files and HTML setup for daily exercises

---
+++ javascript/Daily/day_3.js
@@ -0,0 +1,29 @@
+let arr = [1,2,45, 44, 0, 100,3,4,5];
+// // arr.push(6);
+// // console.log(arr);
+// // console.log(arr.length);
+
+// // console.log(arr.slice(1,4));
+// // console.log(arr.toString());
+// // console.log(arr.shift());
+
+// let d = arr.map(x => x*2);
+// console.log(d)
+
+// let arr = ["hello", "of", "a", "ghsadkjhsl", "usdff"];
+// let fil = arr.filter((word) => word.length > 3);
+// console.log(fil);
+
+// console.log(arr.findIndex(x => x ==2));
+// console.log(arr.includes(76));
+// console.log(arr.unshift(10));
+// console.log(arr.splice(0,2,23));
+// console.log(arr.concat(7,8,9));
+// console.log(arr);
+
+// let arr = ["uydsuf", "bdsuybf","vusbdc", "nausdb"];
+// console.log(arr.join("-"));
+arr.sort();
+console.log(arr);
+console.log(arr.sort((a, b) => a - b));
+
